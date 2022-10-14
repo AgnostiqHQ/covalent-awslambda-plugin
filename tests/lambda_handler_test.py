8c00a91eca39bce6b9783f30c27f64b918c182d0
Venkat Bala <venkat@agnostiq.ai>
2022-10-14 19:25:33 +0000

    pre-commit ci fixes

--- tests/lambda_handler_test.py
+++ tests/lambda_handler_test.py
@@ -20,8 +20,10 @@
 
 import os
 from unittest.mock import MagicMock
-from covalent_awslambda_plugin.exec import handler
+
 import pytest
+
+from covalent_awslambda_plugin.exec import handler
 
 
 @pytest.fixture
@@ -29,8 +31,9 @@ def event():
     return {
         "S3_BUCKET_NAME": "test",
         "COVALENT_TASK_FUNC_FILENAME": "test_function.pkl",
-        "RESULT_FILENAME": "test_result.pkl"
+        "RESULT_FILENAME": "test_result.pkl",
     }
+
 
 def test_assert_os_environ_home(mocker, event):
     os_environ_mock = mocker.patch("covalent_awslambda_plugin.exec.os.environ")
@@ -38,7 +41,10 @@ def test_assert_os_environ_home(mocker, event):
     mocker.patch("covalent_awslambda_plugin.exec.os.path.join")
     mocker.patch("covalent_awslambda_plugin.exec.boto3.client", return_value=MagicMock())
     mocker.patch("covalent_awslambda_plugin.exec.open")
-    mocker.patch("covalent_awslambda_plugin.exec.pickle.load", return_value=(MagicMock(), MagicMock(), MagicMock()))
+    mocker.patch(
+        "covalent_awslambda_plugin.exec.pickle.load",
+        return_value=(MagicMock(), MagicMock(), MagicMock()),
+    )
     mocker.patch("covalent_awslambda_plugin.exec.pickle.dump")
 
     # invoke the handler
@@ -46,13 +52,17 @@ def test_assert_os_environ_home(mocker, event):
 
     os_environ_mock.__setitem__.assert_called_with("HOME", "/tmp")
 
+
 def test_assert_os_chdir_tmp(mocker, event):
     os_chdir_mock = mocker.patch("covalent_awslambda_plugin.exec.os.chdir")
     mocker.patch("covalent_awslambda_plugin.exec.os.environ")
     mocker.patch("covalent_awslambda_plugin.exec.os.path.join")
     mocker.patch("covalent_awslambda_plugin.exec.boto3.client", return_value=MagicMock())
     mocker.patch("covalent_awslambda_plugin.exec.open")
-    mocker.patch("covalent_awslambda_plugin.exec.pickle.load", return_value=(MagicMock(), MagicMock(), MagicMock()))
+    mocker.patch(
+        "covalent_awslambda_plugin.exec.pickle.load",
+        return_value=(MagicMock(), MagicMock(), MagicMock()),
+    )
     mocker.patch("covalent_awslambda_plugin.exec.pickle.dump")
 
     # invoke the handler
@@ -60,17 +70,22 @@ def test_assert_os_chdir_tmp(mocker, event):
 
     os_chdir_mock.assert_called_with("/tmp")
 
+
 def test_assert_s3_bucket_name_keyerror(mocker, event):
     mocker.patch("covalent_awslambda_plugin.exec.os.environ")
     mocker.patch("covalent_awslambda_plugin.exec.os.chdir")
     mocker.patch("covalent_awslambda_plugin.exec.os.path.join")
     mocker.patch("covalent_awslambda_plugin.exec.boto3.client", return_value=MagicMock())
     mocker.patch("covalent_awslambda_plugin.exec.open")
-    mocker.patch("covalent_awslambda_plugin.exec.pickle.load", return_value=(MagicMock(), MagicMock(), MagicMock()))
+    mocker.patch(
+        "covalent_awslambda_plugin.exec.pickle.load",
+        return_value=(MagicMock(), MagicMock(), MagicMock()),
+    )
     mocker.patch("covalent_awslambda_plugin.exec.pickle.dump")
 
     with pytest.raises(KeyError) as r:
         handler({"COVALENT_TASK_FUNC_FILENAME": "test", "RESULT_FILENAME": "test"}, None)
+
 
 def test_assert_covalent_task_filename_keyerror(mocker, event):
     mocker.patch("covalent_awslambda_plugin.exec.os.environ")
@@ -78,11 +93,15 @@ def test_assert_covalent_task_filename_keyerror(mocker, event):
     mocker.patch("covalent_awslambda_plugin.exec.os.path.join")
     mocker.patch("covalent_awslambda_plugin.exec.boto3.client", return_value=MagicMock())
     mocker.patch("covalent_awslambda_plugin.exec.open")
-    mocker.patch("covalent_awslambda_plugin.exec.pickle.load", return_value=(MagicMock(), MagicMock(), MagicMock()))
+    mocker.patch(
+        "covalent_awslambda_plugin.exec.pickle.load",
+        return_value=(MagicMock(), MagicMock(), MagicMock()),
+    )
     mocker.patch("covalent_awslambda_plugin.exec.pickle.dump")
 
     with pytest.raises(KeyError) as r:
         handler({"S3_BUCKET_NAME": "test", "RESULT_FILENAME": "test"}, None)
+
 
 def test_assert_result_filename_keyerror(mocker, event):
     mocker.patch("covalent_awslambda_plugin.exec.os.environ")
@@ -90,7 +109,10 @@ def test_assert_result_filename_keyerror(mocker, event):
     mocker.patch("covalent_awslambda_plugin.exec.os.path.join")
     mocker.patch("covalent_awslambda_plugin.exec.boto3.client", return_value=MagicMock())
     mocker.patch("covalent_awslambda_plugin.exec.open")
-    mocker.patch("covalent_awslambda_plugin.exec.pickle.load", return_value=(MagicMock(), MagicMock(), MagicMock()))
+    mocker.patch(
+        "covalent_awslambda_plugin.exec.pickle.load",
+        return_value=(MagicMock(), MagicMock(), MagicMock()),
+    )
     mocker.patch("covalent_awslambda_plugin.exec.pickle.dump")
 
     with pytest.raises(KeyError) as r:
@@ -103,7 +125,10 @@ def test_assert_os_path_join(mocker, event):
     os_path_join_mock = mocker.patch("covalent_awslambda_plugin.exec.os.path.join")
     mocker.patch("covalent_awslambda_plugin.exec.boto3.client", return_value=MagicMock())
     mocker.patch("covalent_awslambda_plugin.exec.open")
-    mocker.patch("covalent_awslambda_plugin.exec.pickle.load", return_value=(MagicMock(), MagicMock(), MagicMock()))
+    mocker.patch(
+        "covalent_awslambda_plugin.exec.pickle.load",
+        return_value=(MagicMock(), MagicMock(), MagicMock()),
+    )
     mocker.patch("covalent_awslambda_plugin.exec.pickle.dump")
 
     # invoke the handler
@@ -111,13 +136,19 @@ def test_assert_os_path_join(mocker, event):
 
     assert os_path_join_mock.call_count == 2
 
+
 def test_assert_boto3_client(mocker, event):
     mocker.patch("covalent_awslambda_plugin.exec.os.chdir")
     mocker.patch("covalent_awslambda_plugin.exec.os.environ")
     mocker.patch("covalent_awslambda_plugin.exec.os.path.join")
-    boto3_client_mock = mocker.patch("covalent_awslambda_plugin.exec.boto3.client", return_value=MagicMock())
+    boto3_client_mock = mocker.patch(
+        "covalent_awslambda_plugin.exec.boto3.client", return_value=MagicMock()
+    )
     mocker.patch("covalent_awslambda_plugin.exec.open")
-    mocker.patch("covalent_awslambda_plugin.exec.pickle.load", return_value=(MagicMock(), MagicMock(), MagicMock()))
+    mocker.patch(
+        "covalent_awslambda_plugin.exec.pickle.load",
+        return_value=(MagicMock(), MagicMock(), MagicMock()),
+    )
     mocker.patch("covalent_awslambda_plugin.exec.pickle.dump")
 
     # invoke the handler
@@ -125,13 +156,19 @@ def test_assert_boto3_client(mocker, event):
 
     boto3_client_mock.assert_called_with("s3")
 
+
 def test_assert_s3_download_file_mock(mocker, event):
     mocker.patch("covalent_awslambda_plugin.exec.os.chdir")
     mocker.patch("covalent_awslambda_plugin.exec.os.environ")
     mocker.patch("covalent_awslambda_plugin.exec.os.path.join")
-    boto3_client_mock = mocker.patch("covalent_awslambda_plugin.exec.boto3.client", return_value=MagicMock())
+    boto3_client_mock = mocker.patch(
+        "covalent_awslambda_plugin.exec.boto3.client", return_value=MagicMock()
+    )
     mocker.patch("covalent_awslambda_plugin.exec.open")
-    mocker.patch("covalent_awslambda_plugin.exec.pickle.load", return_value=(MagicMock(), MagicMock(), MagicMock()))
+    mocker.patch(
+        "covalent_awslambda_plugin.exec.pickle.load",
+        return_value=(MagicMock(), MagicMock(), MagicMock()),
+    )
     mocker.patch("covalent_awslambda_plugin.exec.pickle.dump")
 
     # invoke the handler
@@ -146,7 +183,10 @@ def test_assert_pickle_load_mock(mocker, event):
     mocker.patch("covalent_awslambda_plugin.exec.os.path.join")
     mocker.patch("covalent_awslambda_plugin.exec.boto3.client", return_value=MagicMock())
     mocker.patch("covalent_awslambda_plugin.exec.open")
-    pickle_load_mock = mocker.patch("covalent_awslambda_plugin.exec.pickle.load", return_value=(MagicMock(), MagicMock(), MagicMock()))
+    pickle_load_mock = mocker.patch(
+        "covalent_awslambda_plugin.exec.pickle.load",
+        return_value=(MagicMock(), MagicMock(), MagicMock()),
+    )
     mocker.patch("covalent_awslambda_plugin.exec.pickle.dump")
 
     # invoke the handler
@@ -154,13 +194,17 @@ def test_assert_pickle_load_mock(mocker, event):
 
     pickle_load_mock.assert_called_once()
 
+
 def test_assert_function_call(mocker, event):
     mocker.patch("covalent_awslambda_plugin.exec.os.chdir")
     mocker.patch("covalent_awslambda_plugin.exec.os.environ")
     mocker.patch("covalent_awslambda_plugin.exec.os.path.join")
     mocker.patch("covalent_awslambda_plugin.exec.boto3.client", return_value=MagicMock())
     mocker.patch("covalent_awslambda_plugin.exec.open")
-    pickle_load_mock = mocker.patch("covalent_awslambda_plugin.exec.pickle.load", return_value=(MagicMock(), MagicMock(), MagicMock()))
+    pickle_load_mock = mocker.patch(
+        "covalent_awslambda_plugin.exec.pickle.load",
+        return_value=(MagicMock(), MagicMock(), MagicMock()),
+    )
     mocker.patch("covalent_awslambda_plugin.exec.pickle.dump")
 
     # invoke the handler
@@ -171,13 +215,17 @@ def test_assert_function_call(mocker, event):
 
     pickle_load_mock.return_value[0].assert_called_once_with(*args, **kwargs)
 
+
 def test_assert_pickle_dump(mocker, event):
     mocker.patch("covalent_awslambda_plugin.exec.os.chdir")
     mocker.patch("covalent_awslambda_plugin.exec.os.environ")
     mocker.patch("covalent_awslambda_plugin.exec.os.path.join")
     mocker.patch("covalent_awslambda_plugin.exec.boto3.client", return_value=MagicMock())
     mocker.patch("covalent_awslambda_plugin.exec.open")
-    mocker.patch("covalent_awslambda_plugin.exec.pickle.load", return_value=(MagicMock(), MagicMock(), MagicMock()))
+    mocker.patch(
+        "covalent_awslambda_plugin.exec.pickle.load",
+        return_value=(MagicMock(), MagicMock(), MagicMock()),
+    )
     pickle_dump_mock = mocker.patch("covalent_awslambda_plugin.exec.pickle.dump")
 
     # invoke the handler
@@ -185,24 +233,22 @@ def test_assert_pickle_dump(mocker, event):
 
     pickle_dump_mock.assert_called_once()
 
+
 def test_assert_s3_upload_file(mocker, event):
     mocker.patch("covalent_awslambda_plugin.exec.os.chdir")
     mocker.patch("covalent_awslambda_plugin.exec.os.environ")
     mocker.patch("covalent_awslambda_plugin.exec.os.path.join")
-    s3_client_mock = mocker.patch("covalent_awslambda_plugin.exec.boto3.client", return_value=MagicMock())
+    s3_client_mock = mocker.patch(
+        "covalent_awslambda_plugin.exec.boto3.client", return_value=MagicMock()
+    )
     mocker.patch("covalent_awslambda_plugin.exec.open")
-    mocker.patch("covalent_awslambda_plugin.exec.pickle.load", return_value=(MagicMock(), MagicMock(), MagicMock()))
+    mocker.patch(
+        "covalent_awslambda_plugin.exec.pickle.load",
+        return_value=(MagicMock(), MagicMock(), MagicMock()),
+    )
     mocker.patch("covalent_awslambda_plugin.exec.pickle.dump")
 
     # invoke the handler
     handler(event, None)
 
     s3_client_mock.return_value.upload_file.assert_called_once()
-
-
-
-
-
-
-
-
