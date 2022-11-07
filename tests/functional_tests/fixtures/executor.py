5606972b166352076e0c535dfdb502d38b3ad4df
Alejandro Esquivel <ae@alejandro.ltd>
2022-11-07 14:37:12 -0800

    Updated executor config

--- tests/functional_tests/fixtures/executor.py
+++ tests/functional_tests/fixtures/executor.py
@@ -7,6 +7,8 @@ import os
 from covalent_awslambda_plugin import AWSLambdaExecutor
 
 executor_config = {
+    "function_name": os.getenv("executor_function_name"),
+    "execution_role": os.getenv("executor_execution_role"),
     "s3_bucket_name": os.getenv("executor_s3_bucket_name"),
     "poll_freq": os.getenv("executor_poll_freq", 5),
     "timeout": os.getenv("executor_timeout", 60),
